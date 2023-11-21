baa12f36eaf867e7f1ce43fce6c3822e1f678f13
slamdunk7575 <slamdunk7575@gmail.com>
2023-11-21 17:50:32 +0900

    (feat) init commit

---
+++ src/main/java/me/test/stock/ConcurrencyStockApplication.java
@@ -0,0 +1,13 @@
+package me.test.stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrencyStockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcurrencyStockApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/test/stock/ConcurrencyStockApplicationTests.java
@@ -0,0 +1,13 @@
+package me.test.stock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrencyStockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
