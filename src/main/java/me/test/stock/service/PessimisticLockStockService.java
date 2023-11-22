2b2dcd6dbd188a2236874a94ef213a5b3c52519b
slamdunk7575 <slamdunk7575@gmail.com>
2023-11-22 18:02:53 +0900

    (refactor) Pessimistic Lock 이용하여 재고 감소 동시성 이슈 해결

---
+++ src/main/java/me/test/stock/service/PessimisticLockStockService.java
@@ -0,0 +1,23 @@
+package me.test.stock.service;
+
+import me.test.stock.domain.Stock;
+import me.test.stock.repository.StockRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class PessimisticLockStockService {
+
+    private final StockRepository stockRepository;
+
+    public PessimisticLockStockService(StockRepository stockRepository) {
+        this.stockRepository = stockRepository;
+    }
+
+    @Transactional
+    public void decrease(Long id, Long quantity) {
+        Stock stock = stockRepository.findByIdWithPessimisticLock(id);
+
+        stock.decrease(quantity);
+    }
+}
--- src/test/java/me/test/stock/service/StockServiceTest.java
+++ src/test/java/me/test/stock/service/StockServiceTest.java
@@ -16,7 +16,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 class StockServiceTest {
 
     @Autowired
-    private StockService stockService;
+    private PessimisticLockStockService stockService;
 
     @Autowired
     private StockRepository stockRepository;
